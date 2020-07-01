fa7727ac82447a97b4318c868e3413528bfe91dd
Mehul1103 <mehulsancheti0212@gmail.com>
2020-07-01 16:34:29 -0400

    feat:Speaker rest api controllers

--- src/main/java/com/demo/conference/controllers/SessionController.java
+++ src/main/java/com/demo/conference/controllers/SessionController.java
@@ -2,12 +2,16 @@ package com.demo.conference.controllers;
 
 import java.util.List;
 
+import org.apache.catalina.connector.Request;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.conference.models.Session;
@@ -34,5 +38,17 @@ public class SessionController {
 	@PostMapping
 	public Session create(@RequestBody final Session session) {
 		return sessionRepository.saveAndFlush(session);
+	}
+	
+	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable Long id) {
+		sessionRepository.deleteById(id);
+	}
+	
+	@RequestMapping(value = "{id}", method = RequestMethod.PUT)
+	public Session update(@PathVariable Long id, @RequestBody Session session) {
+		Session currentSession = sessionRepository.getOne(id);
+		BeanUtils.copyProperties(session, currentSession, "session_id");
+		return sessionRepository.saveAndFlush(currentSession);
 	}
 }
--- src/main/java/com/demo/conference/controllers/SpeakerController.java
+++ src/main/java/com/demo/conference/controllers/SpeakerController.java
@@ -1,5 +1,54 @@
 package com.demo.conference.controllers;
 
-public class SpeakerController {
+import java.util.List;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.conference.models.Speaker;
+import com.demo.conference.repositories.SpeakerRepository;
+
+@RestController
+@RequestMapping("api/v1/speaker")
+public class SpeakerController {
+	
+	@Autowired
+	private SpeakerRepository speakerRepository;
+	
+	@GetMapping
+	public List<Speaker> list(){
+		return speakerRepository.findAll();
+	}
+	
+	@GetMapping
+	@RequestMapping("{id}")
+	public Speaker get(@PathVariable Long id) {
+		return speakerRepository.getOne(id);
+	}
+	
+	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable Long id) {
+		speakerRepository.deleteById(id);
+	}
+	
+	@PostMapping
+	public Speaker create(@RequestBody final Speaker speaker) {
+		return speakerRepository.saveAndFlush(speaker);
+	}
+	
+	@RequestMapping(value = "{id}", method = RequestMethod.PUT)
+	public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker) {
+		Speaker currentSpeaker = speakerRepository.getOne(id);
+		BeanUtils.copyProperties(speaker, currentSpeaker, "speaker_id");
+		return speakerRepository.saveAndFlush(currentSpeaker);
+	}
+	
 }
